fdd606fb7e40353921ef01ea0ce7a4d13364d977
hyunsookim <97166760+khs0415p@users.noreply.github.com>
2023-03-17 13:11:11 +0900

    [unrated] Title: 미로 탈출 명령어, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -4,31 +4,34 @@ sys.setrecursionlimit(10**8)
 
 dx = [1, 0, 0, -1]
 dy = [0, -1, 1, 0]
-dAlpha = ['d', 'l', 'r', 'u']
-answer = "z"
-
-
-def isVaild(nx, ny, n, m):
-    return 1 <= nx and nx <= n and 1 <= ny and ny <= m
-
-
-def dfs(n, m, x, y, r, c, prev_s, cnt, k):
-    global answer
-    if k < cnt + abs(x - r) + abs(y - c):
-        return
-    if x == r and y == c and cnt == k:
-        answer = prev_s
-        return
-    for i in range(4):
-        if isVaild(x + dx[i], y + dy[i], n, m) and prev_s < answer:
-            dfs(n, m, x + dx[i], y + dy[i], r, c, prev_s+dAlpha[i], cnt + 1, k)
+da = ['d', 'l', 'r', 'u']
 
 
 def solution(n, m, x, y, r, c, k):
-    dist = abs(x - r) + abs(y - c)
-    if dist > k or (k - dist) % 2 == 1:
+    
+    if (k - abs(x-r) + abs(y-c))%2 or abs(x-r) + abs(y-c) > k:
         return "impossible"
-
-    dfs(n, m, x, y, r, c, "", 0, k)
-
+    
+    
+    def dfs(n, m, x, y, r, c, prev_s, k):
+        nonlocal answer
+        
+        if k < len(prev_s) + abs(x-r) + abs(y-c):
+            return
+        
+        if x == r and y == c and len(prev_s) == k :
+            answer = prev_s
+            return
+        
+        for i in range(4):
+            
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 < nx <= n and 0 < ny <= m and prev_s < answer:
+                dfs(n, m, nx, ny, r, c, prev_s + da[i], k)
+            
+    answer = "z"
+    dfs(n, m, x, y, r, c, "", k)
+    
     return answer
