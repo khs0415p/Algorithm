df882305fad9f781054ebbee7f44193668a8868d
hyunsookim <97166760+khs0415p@users.noreply.github.com>
2023-04-20 11:00:49 +0900

    [Gold IV] Title: 테트로미노, Time: 7560 ms, Memory: 37996 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+result = float("-inf")
+
+def dfs(depth, x, y, visited, total):
+    global result
+    if depth == 4:
+        result = max(result, total)
+        return
+    
+    for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+        nx = x + dx
+        ny = y + dy
+        
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+            if depth == 2:
+                visited[nx][ny] = 1
+                dfs(depth + 1, x, y, visited, total + board[nx][ny])
+                visited[nx][ny] = 0
+                
+            visited[nx][ny] = 1
+            dfs(depth + 1, nx, ny, visited, total + board[nx][ny])
+            visited[nx][ny] = 0
+
+visited = [[0] * M for _ in range(N)]
+for i in range(N):
+    for j in range(M):
+        visited[i][j] = 1
+        dfs(1, i, j, visited, board[i][j])
+        visited[i][j] = 0
+
+print(result)
