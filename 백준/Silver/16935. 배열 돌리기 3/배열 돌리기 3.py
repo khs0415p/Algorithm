8cb2e74c49f0fdd20f20144e4bd4acdbe3b1ea9c
hyunsookim <97166760+khs0415p@users.noreply.github.com>
2023-04-25 19:47:33 +0900

    [Silver I] Title: 배열 돌리기 3, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16935. 배열 돌리기 3/배열 돌리기 3.py
+++ 백준/Silver/16935. 배열 돌리기 3/배열 돌리기 3.py
@@ -1,112 +1,77 @@
-# 상하반전
-# 좌우반적
-# 오른쪽 90도
-# 왼쪽 90도
-# 5, 6번 연산을 수행하려면 배열을 크기가 N/2×M/2인 4개의 부분 배열로 나눠야 한다. 아래 그림은 크기가 6×8인 배열을 4개의 그룹으로 나눈 것이고, 1부터 4까지의 수로 나타냈다.
-# 5번 연산은 1번 그룹의 부분 배열을 2번 그룹 위치로, 2번을 3번으로, 3번을 4번으로, 4번을 1번으로 이동시키는 연산이다.
-# 6번 연산은 1번 그룹의 부분 배열을 4번 그룹 위치로, 4번을 3번으로, 3번을 2번으로, 2번을 1번으로 이동시키는 연산이다.
 import sys
 
-def up_down(board):
-    n, m = len(board), len(board[0])
-    new_board = [[0] * m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            new_board[n-i-1][j] = board[i][j]
-            
-    return new_board
+def swap(x, y):
+    global stat
+    temp = stat[x]
+    stat[x] = stat[y]
+    stat[y] = temp
 
-def left_right(board):
-    n, m = len(board), len(board[0])
-    new_board = [[0] * m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            new_board[i][m-j-1] = board[i][j]
-            
-    return new_board
+def rot(arr, turn, flip):
+    if flip:
+        for i in range(len(arr)):
+            arr[i] = arr[i][::-1]
+    for _ in range(turn):
+        arr = list(map(list, zip(*arr[::-1])))
+    return arr
 
-def rotation(board, isLeft=False):
-    n, m = len(board), len(board[0])
-    new_board = [[0] * n for _ in range(m)]
-    
-    if isLeft:
-        
-        for i in range(n):
-            for j in range(m):
-                new_board[m-j-1][i] = board[i][j]
-        
-    else:
-        for i in range(n):
-            for j in range(m):
-                new_board[j][n-i-1] = board[i][j]
-            
-    return new_board
-
-def transfer(board, isReverse=False):
-    n, m = len(board), len(board[0])
-    new_board = [[0] * m for _ in range(n)]
-    row_half, col_half = n//2, m//2
-    
-    if isReverse:
-        for i in range(n):
-            for j in range(m):
-                if 0 <= i < row_half and 0 <= j < col_half:
-                    new_board[i+row_half][j]= board[i][j]
-                    
-                elif 0 <= i < row_half and col_half <= j < m:
-                    new_board[i][j-col_half] = board[i][j]
-                    
-                elif row_half <= i < n and col_half <= j < m:
-                    new_board[i-row_half][j] = board[i][j]
-                    
-                else:
-                    new_board[i][j+col_half] = board[i][j]
-        
-    else:
-        
-        
-        
-        for i in range(n):
-            for j in range(m):
-                if 0 <= i < row_half and 0 <= j < col_half:
-                    new_board[i][j+col_half]= board[i][j]
-                    
-                elif 0 <= i < row_half and col_half <= j < m:
-                    new_board[i+(n//2)][j] = board[i][j]
-                    
-                elif row_half <= i < n and col_half <= j < m:
-                    new_board[i][j-col_half] = board[i][j]
-                    
-                else:
-                    new_board[i-row_half][j] = board[i][j]
-                    
-    return new_board
-
-if __name__ == "__main__":
-    input = sys.stdin.readline
-    n, m, op = map(int, input().split())
-    board = [list(map(int, input().split())) for _ in range(n)]
-    opers = list(map(int, input().split()))
-    
-    for oper in opers:
-        if oper == 1:
-            board = up_down(board)
-            
-        elif oper == 2:
-            board = left_right(board)
-            
-        elif oper == 3:
-            board = rotation(board)
-            
-        elif oper == 4:
-            board = rotation(board, True)
-            
-        elif oper == 5:
-            board = transfer(board)
-            
-        else:
-            board = transfer(board, True)
-            
-    for b in board:
-        print(*b)
-        
+N, M, R = map(int, sys.stdin.readline().rstrip().split())
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, sys.stdin.readline().rstrip().split())))
+stat = [0, 1, 2, 3, 4, 0]
+Rs = list(map(int, sys.stdin.readline().rstrip().split()))
+for work in Rs:
+    if work == 1:
+        swap(1, 4)
+        swap(2, 3)
+        if stat[0] == 0:
+            stat[0] = 2
+        elif stat[0] == 2:
+            stat[0] = 0
+        stat[5] = 1 - stat[5]
+    elif work == 2:
+        swap(1, 2)
+        swap(3, 4)
+        if stat[0] == 1:
+            stat[0] = 3
+        elif stat[0] == 3:
+            stat[0] = 1
+        stat[5] = 1 - stat[5]
+    elif work == 3 or work == 5:
+        temp = stat[4]
+        stat[4] = stat[3]
+        stat[3] = stat[2]
+        stat[2] = stat[1]
+        stat[1] = temp
+        if work == 3:
+            stat[0] += 1
+            if stat[0] == 4:
+                stat[0] = 0
+    elif work == 4 or work == 6:
+        temp = stat[1]
+        stat[1] = stat[2]
+        stat[2] = stat[3]
+        stat[3] = stat[4]
+        stat[4] = temp
+        if work == 4:
+            stat[0] -= 1
+            if stat[0] == -1:
+                stat[0] = 3
+arrs = [[],[],[],[]]
+for i in range(N//2):
+    arrs[0].append(arr[i][:M//2])
+    arrs[1].append(arr[i][M//2:])
+for i in range(N//2, N):
+    arrs[3].append(arr[i][:M//2])
+    arrs[2].append(arr[i][M//2:])
+new_arrs = [[],[],[],[]]
+for i in range(4):
+    new_arrs[i] = rot(arrs[stat[i+1]-1], stat[0], stat[5])
+if stat[0] % 2:
+    x = M // 2
+else:
+    x = N // 2
+for i in range(x):
+    print(*(new_arrs[0][i] + new_arrs[1][i]))
+for i in range(x):
+    print(*(new_arrs[3][i] + new_arrs[2][i]))
