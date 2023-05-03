7cc00c846e0f0bf79953e177e0bde6ca1fc6932f
hyunsookim <97166760+khs0415p@users.noreply.github.com>
2023-05-03 13:39:45 +0900

    [Gold III] Title: 컬러볼, Time: 636 ms, Memory: 84060 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.py
+++ 백준/Gold/10800. 컬러볼/컬러볼.py
@@ -1,32 +1,36 @@
 import sys
-from collections import defaultdict
 
 def main(n):
+    
     balls = []
     for i in range(n):
-        c, w = map(int, input().split())
-        balls.append((w, c, i))
+        c, s = map(int, input().split())
+        balls.append((s, c, i))
     balls.sort()
     
-    color_weight = defaultdict(int)
-    weight_weight = defaultdict(int)
-    prefix_sum = [0] * (n+1)
-    res = [0] * (n+1)
-    
-    for i in range(1, n+1):
-        prefix_sum[i] = (prefix_sum[i-1] + balls[i-1][0])
-        if i > 0 and balls[i-2][0] == balls[i-1][0] and balls[i-2][1] == balls[i-1][1]:
-            res[balls[i-1][2]] = res[balls[i-2][2]]
-            
-        else:
-            res[balls[i-1][2]] = prefix_sum[i-1] - color_weight[balls[i-1][1]] - weight_weight[balls[i-1][0]]
-        
-        
-        color_weight[balls[i-1][1]] += balls[i-1][0]
-        weight_weight[balls[i-1][0]] += balls[i-1][0]
-    
+    stack = []
+    total = prev = 0
+    answer = [0] * n
+    color_sum = [0] * (n + 1)
     for i in range(n):
-        print(res[i])   
+        size, color, index = balls[i]
+        if size == prev:
+            answer[index] = total - color_sum[color]
+            stack.append((size, color))
+            continue
+        
+        for s_s, s_c in stack:
+            total += s_s
+            color_sum[s_c] += s_s
+        
+        stack = [(size, color)]
+        answer[index] = total - color_sum[color]
+        prev = size
+    print(*answer, sep='\n')
+            
+            
+        
+    
     
 
 if __name__ == "__main__":
