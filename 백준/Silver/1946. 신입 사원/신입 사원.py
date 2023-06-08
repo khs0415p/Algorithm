14ac2dafa3eb2df0877fc27bad2428ac17c25de3
hyunsookim <97166760+khs0415p@users.noreply.github.com>
2023-06-08 19:38:05 +0900

    [Silver I] Title: 신입 사원, Time: 1596 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -2,18 +2,30 @@ import sys
 
 def main(m):
     
-    arr = [tuple(map(int, input().split()))for _ in range(m)]
-    arr.sort()
+    # arr = [tuple(map(int, input().split()))for _ in range(m)]
+    # arr.sort()
     
-    stack = []
-    cnt = m
-    for i in range(m):
-        if not stack or stack[-1][1] > arr[i][1]:
-            stack.append(arr[i])
+    # stack = []
+    # cnt = m
+    # for i in range(m):
+    #     if not stack or stack[-1][1] > arr[i][1]:
+    #         stack.append(arr[i])
         
-        elif stack[-1][1] < arr[i][1]:
-            cnt -= 1
+    #     elif stack[-1][1] < arr[i][1]:
+    #         cnt -= 1
     
+    rank = [0] * (m+1)
+    for _ in range(m):
+        a, b = map(int, input().split())
+        rank[a] = b
+    
+    cnt = 1
+    high = rank[1]
+    for i in range(2, m+1):
+        if high > rank[i]:
+            cnt += 1
+            high = rank[i]
+        
     return cnt
         
 
