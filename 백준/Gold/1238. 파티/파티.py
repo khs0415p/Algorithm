21f1c98e3cfe33f2ffe87a3cb74a3c8e8637e0a1
hyunsookim <97166760+khs0415p@users.noreply.github.com>
2023-06-09 10:11:01 +0900

    [Gold III] Title: 파티, Time: 60 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,48 +1,33 @@
 import sys
-from collections import defaultdict
-import heapq
-
-def main(n, m, x):
-    
-    # n : 마을
-    # m : 길
-    # x : 파티 장소
-    
-    def dijkstra(start):
-        distance = [float("inf")] * (n+1)
-        distance[start] = 0
-        
-        queue = [[0, start]]
-        while queue:
-            dist, node = heapq.heappop(queue)
-            
-            if distance[node] < dist: continue
-            
-            for nn, nc in graph[node]:
-                ndist = dist + nc
-                if distance[nn] > ndist:
-                    distance[nn] = ndist
-                    heapq.heappush(queue, [ndist, nn])
-        
-        return distance
-    
-    graph = defaultdict(list)
-    for _ in range(m):
-        s, e, c = map(int, input().split())
-        graph[s].append((e, c))
-    
-    answer = 0
-    come = dijkstra(x)
-    for i in range(1, n+1):
-        if i != x:
-            
-            answer = max(answer, dijkstra(i)[x] + come[i])
-            
-    return answer
-        
+input = sys.stdin.readline
+from heapq import heappop, heappush
 
 
-if __name__ == "__main__":
-    input = sys.stdin.readline
-    n, m, x = map(int, input().split())
-    print(main(n, m, x))
+def dijkstra(b, d):
+    pq = [(0, X)]
+    while pq:
+        t, v = heappop(pq)
+        if d[v] < t:
+            continue
+        for tt, vv in b[v]:
+            if d[vv] <= t+tt:
+                continue
+            d[vv] = t+tt
+            heappush(pq, (t+tt, vv))
+
+
+if __name__ == '__main__':
+    INF = sys.maxsize
+    N, M, X = map(int, input().split())
+    ib = [[] for _ in range(N+1)]
+    ob = [[] for _ in range(N+1)]
+    iid = [INF] * (N+1)
+    ood = [INF] * (N+1)
+    iid[X] = ood[X] = 0
+    for _ in range(M):
+        a, b, t = map(int, input().split())
+        ib[b].append((t, a))
+        ob[a].append((t, b))
+    dijkstra(ib, iid)
+    dijkstra(ob, ood)
+    print(max(sum(tup) for tup in zip(iid[1:], ood[1:])))
