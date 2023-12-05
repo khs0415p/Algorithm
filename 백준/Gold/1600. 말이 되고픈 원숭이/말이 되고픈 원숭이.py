cd6190a8f26586e487354138900b300f98fb2b70
hyunsookim <97166760+khs0415p@users.noreply.github.com>
2023-12-05 12:02:21 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 5832 ms, Memory: 34116 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -1,43 +1,42 @@
-from collections import deque
 import sys
-sys.setrecursionlimit(2000)
+from collections import deque
+
 input = sys.stdin.readline
 
-K = int(input())
-col, row = map(int, input().split())
-visited = [[[0]*(K+1) for _ in range(col)] for _ in range(row)]
-matrix = [list(map(int, input().split())) for _ in range(row)]
+k = int(input())
+w, h = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(h)]
+moves = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+horses = [[2, -1], [2, 1], [1, -2], [1, 2], [-2, -1], [-2, 1], [-1, 2], [-1, -2]]
 
-knight = {'a': (-2,-1), 'b': (-1,-2), 'c':(2,1), 'd':(1,2),'e': (-2,1), 'f': (-1,2), 'g':(2,-1), 'h':(1,-2),
-        'left': (-1,0), 'right': (1,0), 'top': (0,-1), 'bottom': (0,1)}
+visited = [[k+1] * w for _ in range(h)]
+queue = deque([[0, 0, 0, 0]])
 
-def bfs():
+flag = False
+while queue:
+    cur_x, cur_y, cur_k, cur_cnt = queue.popleft()
     
-    queue = deque()
-    queue.append((0,0,0))
-    visited[0][0][0] = 1
+    if cur_x == (h-1) and cur_y == (w-1):
+        flag = True
+        break
+    
+    if cur_k < k:
+        for dx, dy in horses:
+            nx, ny = cur_x + dx, cur_y + dy
+            if 0 > nx or 0 > ny or h <= nx or w <= ny: continue
+            if board[nx][ny] or visited[nx][ny] <= cur_k + 1: continue
+            visited[nx][ny] = cur_k + 1
+            queue.append([nx, ny, cur_k + 1, cur_cnt + 1])
+            
+    for dx, dy in moves:
+        nx, ny = cur_x + dx, cur_y + dy
 
-    while queue:
-        cur_r, cur_c, cur_k = queue.popleft()    
+        if 0 > nx or 0 > ny or h <= nx or w <= ny: continue
+        if board[nx][ny] or visited[nx][ny] <= cur_k: continue        
+        visited[nx][ny] = cur_k
+        queue.append([nx, ny, cur_k, cur_cnt + 1])
 
-        if (cur_r, cur_c) == (row-1, col-1):
-            return visited[cur_r][cur_c][cur_k] - 1
-
-        
-        for key, value in knight.items():
-            nxt_r = cur_r + value[0]
-            nxt_c = cur_c + value[1]
-            nxt_k = cur_k
-            if key not in ['left', 'right', 'top', 'bottom']:
-                if cur_k == K:
-                    continue
-                nxt_k += 1
-                
-            if 0 <= nxt_r < row and 0 <= nxt_c < col and not matrix[nxt_r][nxt_c] and not visited[nxt_r][nxt_c][nxt_k]:
-                visited[nxt_r][nxt_c][nxt_k] = visited[cur_r][cur_c][cur_k] + 1
-                queue.append((nxt_r, nxt_c, nxt_k))
-
-    return -1
-
-
-print(bfs())
+if flag:
+    print(cur_cnt)
+else:
+    print(-1)
