1ad7e516e1634859f59a542aae166bab01a12af5
hyunsookim <97166760+khs0415p@users.noreply.github.com>
2023-12-05 12:12:52 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 2280 ms, Memory: 34116 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -8,35 +8,34 @@ w, h = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(h)]
 moves = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 horses = [[2, -1], [2, 1], [1, -2], [1, 2], [-2, -1], [-2, 1], [-1, 2], [-1, -2]]
-
 visited = [[k+1] * w for _ in range(h)]
-queue = deque([[0, 0, 0, 0]])
 
-flag = False
-while queue:
-    cur_x, cur_y, cur_k, cur_cnt = queue.popleft()
-    
-    if cur_x == (h-1) and cur_y == (w-1):
-        flag = True
-        break
-    
-    if cur_k < k:
-        for dx, dy in horses:
-            nx, ny = cur_x + dx, cur_y + dy
-            if 0 > nx or 0 > ny or h <= nx or w <= ny: continue
-            if board[nx][ny] or visited[nx][ny] <= cur_k + 1: continue
-            visited[nx][ny] = cur_k + 1
-            queue.append([nx, ny, cur_k + 1, cur_cnt + 1])
+def bfs():
+    queue = [[0, 0, 0, 0]]
+    while queue:
+        tmp = []
+        for cur_x, cur_y, cur_k, cur_cnt in queue:
+            if cur_x == (h-1) and cur_y == (w-1):
+                return cur_cnt
+                
+        
+            if cur_k < k:
+                for dx, dy in horses:
+                    nx, ny = cur_x + dx, cur_y + dy
+                    if 0 > nx or 0 > ny or h <= nx or w <= ny: continue
+                    if board[nx][ny] or visited[nx][ny] <= cur_k + 1: continue
+                    visited[nx][ny] = cur_k + 1
+                    tmp.append([nx, ny, cur_k + 1, cur_cnt + 1])
+                
+            for dx, dy in moves:
+                nx, ny = cur_x + dx, cur_y + dy
+
+                if 0 > nx or 0 > ny or h <= nx or w <= ny: continue
+                if board[nx][ny] or visited[nx][ny] <= cur_k: continue
+                visited[nx][ny] = cur_k
+                tmp.append([nx, ny, cur_k, cur_cnt + 1])
             
-    for dx, dy in moves:
-        nx, ny = cur_x + dx, cur_y + dy
+        queue = tmp
+    return -1
 
-        if 0 > nx or 0 > ny or h <= nx or w <= ny: continue
-        if board[nx][ny] or visited[nx][ny] <= cur_k: continue        
-        visited[nx][ny] = cur_k
-        queue.append([nx, ny, cur_k, cur_cnt + 1])
-
-if flag:
-    print(cur_cnt)
-else:
-    print(-1)
+print(bfs())
